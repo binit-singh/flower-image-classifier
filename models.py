a40bc8f83396c75a37e3f311077a4b657b9c3178
binit-singh <binit4you@gmail.com>
2020-05-18 00:13:16 +0530

    Feature: changed name of models

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 # Lib imports
 from torchvision import models
 
-densenet = models.densenet121(pretrained=True)
+dense_net = models.densenet121(pretrained=True)
 vgg = models.vgg13(pretrained=True)
-alexnet = models.alexnet(pretrained=True)
+alex_net = models.alexnet(pretrained=True)
 
-arch_model = {'densenet': densenet, 'vgg': vgg, 'alexnet': alexnet}
+arch_model = {'densenet': dense_net, 'vgg': vgg, 'alexnet': alex_net}
 arch_input = {'densenet': 1024, 'vgg': 25088, 'alexnet': 9216}
