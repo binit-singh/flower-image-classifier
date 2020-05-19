a447aed59ddc477b47fb21412be2f60f6979118a
binit-singh <binit4you@gmail.com>
2020-05-19 23:39:33 +0530

    feature: Removed unused imports

--- train.py
+++ train.py
@@ -2,13 +2,10 @@ import torch
 from torch import nn
 from torch import optim
 from tqdm import tqdm
-import numpy as np
-from PIL import Image
 
 # Local imports
 from get_input_args import get_train_args
 from train_helper import prepare_dataset, get_model, save_checkpoint, get_device
-from workspace_utils import active_session
 
 
 def train():
@@ -26,45 +23,44 @@ def train():
     running_loss = 0
     print_every = 32
 
-    with active_session():
-        for epoch in range(args.epoch):
+    for epoch in range(args.epoch):
 
-            # Train model
-            for images, labels in tqdm(data['train'], desc='Train'):
-                steps += 1
-                images, labels = images.to(device), labels.to(device)
+        # Train model
+        for images, labels in tqdm(data['train'], desc='Train'):
+            steps += 1
+            images, labels = images.to(device), labels.to(device)
 
-                optimizer.zero_grad()
-                logps = model(images)
-                loss = criterion(logps, labels)
-                loss.backward()
-                optimizer.step()
-                running_loss += loss.item()
+            optimizer.zero_grad()
+            logps = model(images)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
 
-                # Validate model
-                if steps % print_every == 0:
-                    test_loss = 0
-                    accuracy = 0
-                    model.eval()
-                    with torch.no_grad():
-                        for images, labels in data['valid']:
-                            images, labels = images.to(device), labels.to(device)
-                            logps = model(images)
-                            loss = criterion(logps, labels)
-                            test_loss += loss.item()
+            # Validate model
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for images, labels in data['valid']:
+                        images, labels = images.to(device), labels.to(device)
+                        logps = model(images)
+                        loss = criterion(logps, labels)
+                        test_loss += loss.item()
 
-                            ps = torch.exp(logps)
-                            top_p, top_class = ps.topk(1, dim=1)
-                            equals = top_class == labels.view(*top_class.shape)
-                            accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-                    print('***' * 10)
-                    print(f"Epoch {epoch + 1}/{args.epoch}.. ")
-                    print(f"Train loss: {running_loss / print_every:.3f}.. ")
-                    print(f"Test loss: {test_loss / len(data['valid']):.3f}.. ")
-                    print(f"Accuracy: {(accuracy / len(data['valid'])) * 100:.2f} %")
-                    print('***' * 10)
-                    running_loss = 0
-                    model.train()
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                print('***' * 10)
+                print(f"Epoch {epoch + 1}/{args.epoch}.. ")
+                print(f"Train loss: {running_loss / print_every:.3f}.. ")
+                print(f"Test loss: {test_loss / len(data['valid']):.3f}.. ")
+                print(f"Accuracy: {(accuracy / len(data['valid'])) * 100:.2f} %")
+                print('***' * 10)
+                running_loss = 0
+                model.train()
 
     # Save the checkpoint
     save_checkpoint(
